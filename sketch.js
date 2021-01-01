46e86a0b6b5ed955e186e2d80d3212700b9bf70b
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2021-01-01 19:54:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,13 +17,21 @@ function setup() {
   world = engine.world;
 
   ground = new Ground(440,height,1200,20); 
+
+  for (var k = 0; k <=width; k = k + 80) {
+    div.push(new Div(k, height-divh/2, 10, divh));
+}
 }
 
 function draw() {
+   
+  background(0,0,0); 
   Engine.update(engine);
-  
-  background(0,0,0);  
+  ground.display();
+  for (var k = 0; k < div.length; k++){
+    div[k].display();
+}
   drawSprites();
   
-  ground.display();
+  
 }
